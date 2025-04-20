bb0e1a13058594be4368a9d6091aacba4b2b5ff6
olga <ola.skorp.developer@gmail.com>
2025-04-20 16:32:14 +0200

    Initial Commit

---
+++ DisneyExpert/DisneyExpertApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyExpertApp.swift
+//  DisneyExpert
+//
+//  Created by Ольга on 20.04.25.
+//
+
+import SwiftUI
+
+@main
+struct DisneyExpertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DisneyExpertTests/DisneyExpertTests.swift
@@ -0,0 +1,17 @@
+//
+//  DisneyExpertTests.swift
+//  DisneyExpertTests
+//
+//  Created by Ольга on 20.04.25.
+//
+
+import Testing
+@testable import DisneyExpert
+
+struct DisneyExpertTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
